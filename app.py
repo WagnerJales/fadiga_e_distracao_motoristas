47012a24d3cd4eb0f91ccd592243dc988fb83e7e
WagnerJales <100356581+WagnerJales@users.noreply.github.com>
2022-07-26 08:48:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def plot_estoque(dataframe, categoria):
 # importando os dados
 dados = pd.read_csv('df2.csv')
 
-st.title('Análise de estoque\n')
+st.title('Análise de ocorrências de Fadiga e Distração\n')
 st.write('Nesse projeto vamos analisar a quantidade de produtos em estoque, por categoria,  de uma base de dados de produtos de supermercado')
 
 # filtros para a tabela
@@ -76,7 +76,8 @@ Considerando o eixo "x" como as horas ao longo dia "normalizada" (ou seja, varia
 Entre os tipos de ocorrência, apenas o "bocejo" possui eventos no pico da tarde sendo este superior ao primeiro, sua maior frequência são em baixas velocidades com tendência de redução a medida que se aumenta esta variável.
 "Sonolência N1" contrabalanceia as ocorrências de "bocejo" concentrando as suas no pico da manhã quase não existindo no pico da tarde mas em contra-partida, em termos de velocidade, concetra 2 "picos"; em 32km/h e em 52km/h.
 A "oclusão" possui eventos no "entre picos", não obedecendo ao padrão dos demais.
-"olhando para baixo N2" tem características similares a "Sonolência N1" em relação a distribuição de eventos ao longo do dia, e em relação a velocidade, concentra suas ocorrências em baixas velocidades e vai decaindo a medida que a velocidade aumenta.
+"olhando para baixo N2" tem características similares a "Sonolência N1" em relação a distribuição de eventos ao longo do dia, e em relação a velocidade, 
+concentra suas ocorrências em baixas velocidades e vai decaindo a medida que a velocidade aumenta.
 O caso de "Sonolência N2", unico registro do período, ocorre no pico da manhã e com velocidade acima da média.
 
 """
@@ -98,26 +99,34 @@ px.scatter(df2, x="hora", y="Velocidade", color = "Eventos", size = "Velocidade"
 
 **Em relação a forma de captação da informação de fadiga**
 
-O corpo manifesta de diversas a condição da fadiga/distraçãoo mas, apesar da experiência humana ser algo complexo, a tecnologia se esforça na busca de captar manifestações faciais que possam ser correlacionadas com a incapacidade do motorista de exercer a sua função, não capta necessariamente o que acontece "por dentro" pois nem toda expressão facial considerada na tecnologia é expressão da incapacidade do motorista em excercer a sua atividade. Mas esse fato não invalida o uso da tecnologia.
+O corpo manifesta de diversas a condição da fadiga/distraçãoo mas, apesar da experiência humana ser algo complexo, a tecnologia se esforça na busca de captar 
+manifestações faciais que possam ser correlacionadas com a incapacidade do motorista de exercer a sua função, não capta necessariamente o que acontece "por 
+dentro" pois nem toda expressão facial considerada na tecnologia é expressão da incapacidade do motorista em excercer a sua atividade. Mas esse fato não 
+invalida o uso da tecnologia.
 
-O sistema de detecção de fadiga/distração pode ser considerado a "última barreira" para prevenção de acidentes mas uma barreira eficaz é aquela que sequer permitirá que o motorista incapaz inicie a jornada de trabalho.
+O sistema de detecção de fadiga/distração pode ser considerado a "última barreira" para prevenção de acidentes mas uma barreira eficaz é aquela que sequer 
+permitirá que o motorista incapaz inicie a jornada de trabalho.
 
 
 
 **Sobre o dado 'Olhar para baixo N1'**
 
-Este evento é captado quando o motorista desvia o olhar da frente em 1,5seg e esse tipo de comportamento não caracteriza necessariamente o comportamento de fadiga/distração pois esse desvio é necessário para que o motorista olha o retrovisor em cruzamentos, conversões e paradas em semáforos. 
+Este evento é captado quando o motorista desvia o olhar da frente em 1,5seg e esse tipo de comportamento não caracteriza necessariamente o comportamento de 
+fadiga/distração pois esse desvio é necessário para que o motorista olha o retrovisor em cruzamentos, conversões e paradas em semáforos. 
 
 
 
 **Sobre a 'Oclusão'**
 
-A Oclusão também não é necessiariamente o comportamento de fadiga/distração pois é uma ação voluntária do motorista em inibir o funcionamento da câmera sugerindo ações adiministrativas com a identificação dos motoristas. A oclusão também pode ser falha no equipamento.
+A Oclusão também não é necessiariamente o comportamento de fadiga/distração pois é uma ação voluntária do motorista em inibir o funcionamento da câmera 
+sugerindo ações adiministrativas com a identificação dos motoristas. A oclusão também pode ser falha no equipamento.
 
 
 **Em relação aos dias da semana**
 
-A chamada "lei do motorista" (13.103/2015) prevê tempo descanço no interjornada para os motoristas mas é necessário expandir o conceito pois qualquer esforço prolongado que o motorista execute no final-de-semana que não seja necessariamente digirir, está sujeito a ter o mesmo efeito no que diz repeito a coloca-lo numa condição de fadiga. 
+A chamada "lei do motorista" (13.103/2015) prevê tempo descanço no interjornada para os motoristas mas é necessário expandir o conceito pois qualquer esforço 
+prolongado que o motorista execute no final-de-semana que não seja necessariamente digirir, está sujeito a ter o mesmo efeito no que diz repeito a coloca-lo 
+numa condição de fadiga. 
 
 É necessário observar se de fato os motoristas do regime administrativo (que operam de segunda a sexta) estão cumprindo o repouso necessário no final de semana para que não começem a jornada na segunda-feita já fadigados. Para isso pode-se associar testes de atenção antes do inicio da jornada, especialmente para estes casos.
 
